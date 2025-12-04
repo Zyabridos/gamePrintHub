1db359a3eae28db80b8b69fb67c132fbf1938501
Nina Zyabrina <zyabrina95@gmail.com>
2025-12-04 14:11:49 +0100

    fix(infra): ensure API routing works in production/create common env-file

--- apps/frontend/lib/sanityImage.ts
+++
@@ -1,8 +0,0 @@
-import imageUrlBuilder from "@sanity/image-url";
-import sanityClient from "./sanityClient";
-
-const builder = imageUrlBuilder(sanityClient as any);
-
-export function urlForImage(source: any) {
-  return builder.image(source);
-}
